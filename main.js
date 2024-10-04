b4b7211ec6756dee61fe6312a1b1c7ac09bb23a7
SomeDiedYoung <vladimer.osmanovi@Gau.edu.ge>
2024-10-04 12:39:33 +0400

    Adding html.css / index.html / main.js

---
+++ main.js
@@ -0,0 +1,54 @@
+// 1 task 
+
+const Yey = Math.random();
+
+console.log(Yey);
+
+function RandomCeil(){
+    return max = Math.ceil(Yey);
+}
+
+console.log("Task 1: "+RandomCeil());
+
+
+//  2 task 
+
+
+
+function RandomFloor(){
+    return min = Math.floor(Yey);
+}
+
+console.log("Task 2: "+RandomFloor());
+
+
+// 3 task 
+
+
+
+function RandomRound(){
+    return round = Math.round(Yey);
+}
+
+console.log("Task 3: "+RandomRound());
+
+
+// 4 task 
+
+
+function Task_4(a,b){
+    return b ? Math.ceil(a) : Math.floor(a);
+}
+
+;
+console.log("Task 4: "+Task_4(5.53,true));
+
+
+//5 task 
+
+function Task_5(){
+    console.log("Task 5: "+Math.random())
+}
+Task_5()
+
+
